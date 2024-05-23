3cc80a31022089d3212dd0fe91a1ac2cb1dff694
Sefrion <ilyenkov96@gmail.com>
2024-05-23 17:13:27 +0300

    more helper functions added

--- helpers.php
+++ helpers.php
@@ -43,3 +43,29 @@ function loadPartial($name)
     echo "View '{$name}' not found!";
   }
 };
+
+/**
+ * Inspect value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
