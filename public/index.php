1ff63fca76324af3aa5164ac57d03562865ea8f1
Sefrion <ilyenkov96@gmail.com>
2024-05-24 17:58:18 +0300

    created Database class and established connection to mysql

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
 require '../helpers.php';
-
+// Database
+require basePath('Database.php');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+// Router
 require basePath('Router.php');
-
 $router = new Router();
 $routes = require basePath('routes.php');
-
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
-
 $router->route($method, $uri);
