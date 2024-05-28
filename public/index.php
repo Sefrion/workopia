1e0712433ece5b1b562287ae2d36b053a4902c76
Sefrion <ilyenkov96@gmail.com>
2024-05-28 11:27:28 +0300

    fetch single listing and refactor db query to take in params

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ require basePath('Database.php');
 require basePath('Router.php');
 $router = new Router();
 $routes = require basePath('routes.php');
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 $router->route($method, $uri);
