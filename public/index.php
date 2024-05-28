7af129cce53af73b4ab734ee07cbb95b28734cad
Sefrion <ilyenkov96@gmail.com>
2024-05-28 12:02:16 +0300

    folder structure changed, psr-4 autoloader was implemented and namespaces were added

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
-// Database
-require basePath('Database.php');
-// Router
-require basePath('Router.php');
+
+use Framework\Router;
+// spl_autoload_register(function ($class) {
+//   $path = basePath('Framework/' . $class . '.php');
+//   if (file_exists($path)) {
+//     require $path;
+//   }
+// });
+
 $router = new Router();
 $routes = require basePath('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
