ce3f823d5b7547b602bca366710ae14bfe622f5d
Sefrion <ilyenkov96@gmail.com>
2024-05-31 10:33:22 +0300

    added ability to create a listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -65,7 +65,7 @@ class ListingController
 
     $newListingData = array_map('sanitize', $newListingData);
 
-    $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+    $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
 
     $errors = [];
 
@@ -83,7 +83,30 @@ class ListingController
       ]);
     } else {
       // Submit data
-      echo 'Success';
+      $fields = [];
+
+      foreach ($newListingData as $field => $value) {
+        $fields[] = $field;
+      }
+
+      $fields = implode(', ', $fields);
+
+      $values = [];
+
+      foreach ($newListingData as $field => $value) {
+        if ($value === '') {
+          $newListingData[$field] = null;
+        }
+        $values[] = ':' . $field;
+      }
+
+      $values = implode(', ', $values);
+
+      $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+      $this->db->query($query, $newListingData);
+
+      redirect('/listings');
     }
   }
 }
